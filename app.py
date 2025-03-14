072c7fca5e346af5e557951aa45f470a197dc6c0
Gurinder <notifications3972@gmail.com>
2025-03-14 16:27:06 -0700

    auth

--- app.py
+++ app.py
@@ -13,9 +13,9 @@ import geemap
 ee.Authenticate()
 #ee.Initialize()
 
-service_account = 'notifications3972@gmail.com'
-creds = ee.ServiceAccountCredentials(service_account, 'C:/Users/Sonu/AppData/Local/Google/Cloud SDK/google-cloud-sdk/bin/gcloudkey.json')
-ee.Initialize(creds)
+#service_account = 'notifications3972@gmail.com'
+#creds = ee.ServiceAccountCredentials(service_account, 'C:/Users/Sonu/AppData/Local/Google/Cloud SDK/google-cloud-sdk/bin/gcloudkey.json')
+ee.Initialize(project='Earth Engine Default Project')
 
 app = Flask(__name__)
 
