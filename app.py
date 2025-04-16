c23623c03300a8b29cf7e9f8a52d4a5d0382586a
Sonu <notifications3972@gmail.com>
2025-04-15 18:41:59 -0700

    added port listening

--- app.py
+++ app.py
@@ -77,4 +77,5 @@ def geocode():
 # Run the Flask app
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
